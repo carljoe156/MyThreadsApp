e799cc4a81012062ae7e74430278bcb510416c4b
Carl Joseph <carljoseph156@gmail.com>
2025-05-15 16:27:17 -0400

    Export bottom tabs

---
+++ src/app/(tabs)/notifications.tsx
@@ -0,0 +1,4 @@
+import { Text } from "react-native";
+export default function NotificationsScreen() {
+  return <Text>Notifications Screen</Text>;
+}
---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,4 @@
+import { Text } from "react-native";
+export default function ProfileScreen() {
+  return <Text>Profile Screen</Text>;
+}
