4f0f50392a5df3518b328cd4ff532b8fa9d8da3b
Carl Joseph <carljoseph156@gmail.com>
2025-05-16 13:00:44 -0400

    Added Authenication Logic with Supabase and Expo Router

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -12,12 +12,6 @@ export default function HomeScreen() {
           <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
             New Post
           </Link>
-          <Link
-            href="/login"
-            className="text-blue-500 p-4 text-center text-3xl"
-          >
-            Login
-          </Link>
         </>
       )}
     />
