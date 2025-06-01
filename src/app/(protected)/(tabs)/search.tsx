ee84d222c96607a5acadb6466bed85f4fac5754c
Carl Joseph <carljoseph156@gmail.com>
2025-05-31 21:00:05 -0400

    added/Updated Search screen

--- src/app/(protected)/(tabs)/search.tsx
+++ src/app/(protected)/(tabs)/search.tsx
@@ -1,4 +1,26 @@
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 export default function Search() {
-  return <Text>Search</Text>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <View className="flex-1 bg-black p-4">
+      {/* Header */}
+      {/* <Text className="text-white text-2xl font-bold mb-6">Search</Text> */}
+
+      {/* Search Bar */}
+      <View className="flex-row items-center bg-neutral-800 rounded-lg p-3">
+        <Ionicons name="search-outline" size={24} color="white" />
+        <TextInput
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          placeholder="Search"
+          placeholderTextColor="gray"
+          className="text-white ml-3 flex-1"
+        />
+      </View>
+    </View>
+  );
 }
